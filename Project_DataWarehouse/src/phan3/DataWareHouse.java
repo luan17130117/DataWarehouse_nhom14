91f92f312dbf23aee60d46373b7ec2a992555097
RainNy99 <RainNy99@gmail.com>
2020-07-29 17:48:54 +0700

    update phan 3

--- Project_DataWarehouse/src/phan3/DataWareHouse.java
+++ Project_DataWarehouse/src/phan3/DataWareHouse.java
@@ -36,10 +36,12 @@ public class DataWareHouse {
 			con_Control = new GetConnection().getConnection("control");
 			// 2. Lấy các file có trạng thái là OK Staging tại các nhóm có đang active
 			pre_control = con_Control.prepareStatement(
-					"select data_file_logs.id ,data_file_logs.ID_host, data_file_configuaration.data_warehouse_sql,"
-							+ " data_file_configuaration.insert_DW_sql, data_file_configuaration.table_staging_load"
+					"select data_file_logs.id ,data_file_logs.id_config, "
+					+ "data_file_configuaration.data_warehouse_sql,"
+							+ " data_file_configuaration.insert_DW_sql,"
+							+ " data_file_logs.table_staging"
 							+ " from data_file_logs JOIN data_file_configuaration "
-							+ "on data_file_logs.ID_host=data_file_configuaration.id "
+							+ "on data_file_logs.id_config=data_file_configuaration.id "
 							+ "where data_file_configuaration.isActive=1 AND " + condition);
 			// 3. Trả về Result set chứa các record thỏa điều kiện
 			ResultSet re = pre_control.executeQuery();
@@ -51,12 +53,11 @@ public class DataWareHouse {
 				int countEXIST = 0;
 
 				int id_file = re.getInt("id"); // ma file
-				int maGroup = re.getInt("ID_host");// ma group
 				// data_warehouse_sql: mssv, cast([ho] as varchar(100)) + ' ' + cast([ten] as
 				// varchar(100))as hoten,
 				// ngaysinh, gioitinh
 				String sql = re.getString("data_warehouse_sql");// select ***
-				String table_src = re.getString("table_staging_load");// from + table staging
+				String table_src = re.getString("table_staging");// from + table staging
 
 				// 5. Mở connection của database Staging
 				conn_staging = new GetConnection().getConnection("staging");
@@ -74,15 +75,23 @@ public class DataWareHouse {
 				}
 
 				while (re_staging.next()) {// chay tung Record?
-					String id = re_staging.getString("mssv");
-					String full_name = re_staging.getString("hoten");
-					String ngay = re_staging.getString("ngaysinh");
-					
+					String stt = re_staging.getString("STT");
+					String mssv = re_staging.getString("MSSV");
+					String hoLot = re_staging.getString("Ho_lot");
+					String ten = re_staging.getString("Ten");
+					String ngaySinh = re_staging.getString("Ngay_sinh");
+					String maLop = re_staging.getString("Ma_lop");
+					String tenLop = re_staging.getString("Ten_lop");
+					String sdt = re_staging.getString("SDT");
+					String email = re_staging.getString("Email");
+					String queQuan = re_staging.getString("Que_quan");
+					String ghiChu = re_staging.getString("Ghi_chu");
+
 					// chuyen chuoi thanh ngay
 					SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 					java.sql.Date sqlDate = null;
 					try {
-						java.util.Date date = formatter.parse(ngay);
+						java.util.Date date = formatter.parse(ngaySinh);
 						sqlDate = new java.sql.Date(date.getTime());
 
 						// 8. Mở connection database data_warehouse
@@ -90,27 +99,53 @@ public class DataWareHouse {
 						int index_date = getDate(conn_DW1, sqlDate);
 						// 9. Truy xuất SK của sinh viên có mã SV là id và các file của nhóm là maGroup
 						// tại các sinh viên đang active
-						String sql_exceute = "select * from Student where id = '" + id + "' and id_file_group = "
-								+ maGroup + " and isActive = 1";
+						String sql_exceute = "select * from Student where id = '" + mssv + " and isActive = 1";
 						pre_DW = conn_DW1.prepareStatement(sql_exceute);
 						// 10. Trả về ResultSet chứa 1 record thỏa điều kiện truy xuất
 						ResultSet re_DW = pre_DW.executeQuery();
 						int sk_DW = 0;
 						String checkExist = "NO";
-						String nameTemp = null;
-						Date dob_Temp = null;
-						String gender_Temp = null;
-						String id_temp = null;
-						
+
+						String sttTemp = null;
+						String mssvTemp = null;
+						String hoLotTemp = null;
+						String tenTemp = null;
+						Date ngaySinhTemp = null;
+						String maLopTemp = null;
+						String tenLopTemp = null;
+						String sdtTemp = null;
+						String emailTemp = null;
+						String queQuanTemp = null;
+						String ghiChuTemp = null;
+
 						if (re_DW.next()) { // Record?
 							// Yes: Nhánh 11.2
-							id_temp = re_DW.getString("id");
-							nameTemp = re_DW.getString("full_name");
-							dob_Temp = re_DW.getDate("dob");
-							gender_Temp = re_DW.getString("gender");
+							 sttTemp = re_DW.getString("stt");
+							 mssvTemp = re_DW.getString("mssv");
+							 hoLotTemp = re_DW.getString("hoLot");
+							 tenTemp = re_DW.getString("ten");
+							 ngaySinhTemp = re_DW.getDate("ngaySinh");
+							 maLopTemp = re_DW.getString("maLop");
+							 tenLopTemp = re_DW.getString("tenLop");
+							 sdtTemp = re_DW.getString("sdt");
+							 emailTemp = re_DW.getString("email");
+							 queQuanTemp = re_DW.getString("queQuan");
+							 ghiChuTemp = re_DW.getString("ghiChu");
+							 
 							// 11.2.1 So sách các trường còn lại của SV Staging có gì khác không so với
 							// SV trong DataWarehouse không?
-							if (nameTemp.equalsIgnoreCase(full_name) && dob_Temp.equals(sqlDate)) {
+							if (sttTemp.equalsIgnoreCase(stt) && 
+									mssvTemp.equalsIgnoreCase(mssv) && 
+									 hoLotTemp.equalsIgnoreCase(hoLot) && 
+									 tenTemp.equalsIgnoreCase(ten) && 
+									 ngaySinhTemp.equals(sqlDate) && 
+									 maLopTemp.equalsIgnoreCase(maLop) && 
+									 tenLopTemp.equalsIgnoreCase(tenLop) && 
+									 sdtTemp.equalsIgnoreCase(sdt) && 
+									 emailTemp.equalsIgnoreCase(email) && 
+									 queQuanTemp.equalsIgnoreCase(queQuan) &&
+									 ghiChuTemp.equalsIgnoreCase(ghiChu)
+									) {
 								checkExist = "NOCHANGE";// 2 dong y het nhau
 							} else {
 								// 11.2.2.1. Lấy sk của sinh viên đó
@@ -124,23 +159,26 @@ public class DataWareHouse {
 							// *** YES: Tồn tại + có thay đổi: Nhánh 11.2.2
 
 							// 11.2.2.2. In thôn báo thay đổi thông tin SV có mã của nhóm
-							System.out.println("==> Thay đôi TTSV mã: " + id_temp + ", " + nameTemp + ", " + gender_Temp
-									+ ", " + dob_Temp + " , cua nhom " + maGroup + " THANH " + id + ", " + full_name
-									+ ", "  + sqlDate);
+							System.out.println("==> Thay đôi TTSV mã: " + mssvTemp + ", " + hoLotTemp +" "+tenTemp + ", " + ngaySinhTemp
+									+ ", " +  maLopTemp + ", " + tenLopTemp + ", " + sdtTemp + ", " + emailTemp
+									+ ", " + queQuanTemp + ", " +  ghiChuTemp 
+									+ " , cua nhom " + " THANH " +  mssv + ", " + hoLot +" "+ten + ", " + ngaySinh
+									+ ", " +  maLop + ", " + tenLop + ", " + sdt + ", " + email
+									+ ", " + queQuan + ", " +  ghiChu );
 							// 11.2.2.3. Trong database data-warehouse Cập nhật isActive = 0, date_change là
 							// ngày giờ hiện tại
 							value_update += sk_DW + ", ";
 
 							// 11.2.2.4.Thêm dòng SV vào table Student của data_warehouse
 							pre_DW = conn_DW1.prepareStatement(
-									"insert into Student(id, full_name, dob, index_ngaysinh, gender, file_src, id_file_group) values "
-											+ "( '" + id + "', N'" + full_name + "', '" + sqlDate + "', " + index_date
-											+ "', '" + table_src + "', " + maGroup + ")");
-							int i = pre_DW.executeUpdate();
+									"insert into Student(STT, MSSV, Ho_lot, Ten, Ngay_sinh , Ma_lop, Ten_Lop, SDT, Email, Que_quan, Ghi_chu) values "
+											+ "( '" + stt + "', '" +"', N'" + hoLot + "', N'" + ten+ "', '"   + index_date + 
+											"', '"   + maLop+ "', '"   + tenLop + "', '"   + sdt +  "', '"   + email + 
+											"', '"   + queQuan + "', '"   + ghiChu +   ")");
+							
 
-							// value_sql += "( '" + id + "', N'" + full_name + "', '" + sqlDate + "', " +
-							// index_date
-							// + ", N'" + gender + "', '" + table_src + "', " + maGroup + "),";
+							int i = pre_DW.executeUpdate();
+	
 							// 11.2.2.5. tăng số dòng cập nhật lên
 							count_UPDATE++;
 
@@ -148,20 +186,26 @@ public class DataWareHouse {
 							// **** NO: them moi hoan toan: Nhanh 11.1
 
 							// 11.1.1. In thông bao thêm SV
-							System.out.println("==> them moi SV: " + id + ", " + full_name +  ", "
-									+ sqlDate + ", cua nhom " + maGroup);
+							System.out.println("==> them moi SV: " + stt + ", " + hoLot+ "  " +
+											 ten+ ", " +
+											 sqlDate+ ", " +
+											 maLop+ ", " +
+											 tenLop+ ", " +
+											 sdt+ ", " +
+											 email+ ", " +
+											 queQuan+ ", " +
+											 ghiChu+ ", " );
 							// 11.1.2. Thêm thông tin SV chuỗi value_insert
-							value_sql += "( '" + id + "', N'" + full_name + "', '" + sqlDate + "', " + index_date
-									+  "', '" + table_src + "', " + maGroup + "),";
+							value_sql +="( '" + stt + "', '" +"', N'" + hoLot + "', N'" + ten+ "', '"   + index_date + 
+									"', '"   + maLop+ "', '"   + tenLop + "', '"   + sdt +  "', '"   + email + 
+									"', '"   + queQuan + "', '"   + ghiChu +   "),";
 							// 11.1..3. tăng số dòng thêm mới lên
 							count_NEW++;
 
 						} else if (checkExist.equalsIgnoreCase("NOCHANGE")) {
 							// *** NOCHANGE: giong y chang, khong co gi thay doi: Nhanh 11.2.1
 
-							System.out.println("==> KHONG CO GI THAY DOI: TT trong DW" + id_temp + ", " + nameTemp
-									+ ", " + gender_Temp + ", " + dob_Temp + " , cua nhom " + maGroup
-									+ " TT trong Staging " + id + ", " + full_name + ", " + sqlDate);
+							System.out.println("==> KHONG CO GI THAY DOI: TT trong DW");
 							// 11.2.1.1. Tăng số dòng không cần thêm vào data_warehouse lên 1
 							countEXIST++;
 						}
@@ -257,47 +301,42 @@ public class DataWareHouse {
 		}
 		return 0;
 	}
-
-	// chen 1 dong vao DW
-	public boolean insertALine(Connection conn_DW, String sql, String id, String full_name, String gender, Date dob,
-			String table_Staging, int idHostGroup) {
-
-		int i = 0;
-		PreparedStatement pre = null;
-		try {
-			// 11.1.1. Tìm Date_SK của ngày sinh sinh trong Date_dim table
-			pre = conn_DW.prepareStatement("select Date_SK from Date_dim where Full_date like ?");
-			pre.setDate(1, dob);
-			ResultSet re_date = pre.executeQuery();
-			int sk = 0;
-			while (re_date.next()) {
-				sk = re_date.getInt("Date_SK");
-			}
-
-			// 11.1.2. Thêm thông tin cần thiết của SV đó vào Student table của database
-			// data_warehouse
-			pre = conn_DW.prepareStatement("insert into " + sql);
-			pre.setString(1, id);// ma sv
-			pre.setString(2, full_name);// hoten
-			pre.setDate(3, dob);// ngay sinh
-			pre.setInt(4, sk);// index_ngaysinh
-			
-			pre.setString(6, table_Staging);// file_source
-			pre.setInt(7, idHostGroup);
-
-			i = pre.executeUpdate();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		if (i > 0) {
-			return true;
-		}
-		return false;
-	}
-
-
-	
-	
-	
 }
+	// chen 1 dong vao DW
+//	public boolean insertALine(Connection conn_DW, String sql, String id, String full_name, String gender, Date dob,
+//			String table_Staging) {
+//
+//		int i = 0;
+//		PreparedStatement pre = null;
+//		try {
+//			// 11.1.1. Tìm Date_SK của ngày sinh sinh trong Date_dim table
+//			pre = conn_DW.prepareStatement("select Date_SK from Date_dim where Full_date like ?");
+//			pre.setDate(1, dob);
+//			ResultSet re_date = pre.executeQuery();
+//			int sk = 0;
+//			while (re_date.next()) {
+//				sk = re_date.getInt("Date_SK");
+//			}
+//
+//			// 11.1.2. Thêm thông tin cần thiết của SV đó vào Student table của database
+//			// data_warehouse
+//			pre = conn_DW.prepareStatement("insert into " + sql);
+//			pre.setString(1, id);// ma sv
+//			pre.setString(2, full_name);// hoten
+//			pre.setDate(3, dob);// ngay sinh
+//			pre.setInt(4, sk);// index_ngaysinh
+//
+//			pre.setString(6, table_Staging);// file_source
+//
+//			i = pre.executeUpdate();
+//
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		if (i > 0) {
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//}
